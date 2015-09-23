8fef89b09442a90f26e2d9c2ac81c972fbac5c2d
Filip Defar <filip.defar@gmail.com>
2015-09-23 11:20:00 +0200

    Add simple seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+user = User.create( email: 'user@mail.com',
+                    password: 'password',
+                    password_confirmation: 'password' )
+kult = Kult.create( name: 'Cat enthusiasts' )
+
+post1 = Post.create( content: 'I love cats!', user: user, kult: kult )
+post2 = Post.create( content: 'Cats are awesome', user: user, kult: kult )
+
+Comment.create( content: 'I completely agree.', user: user, post: post1 )
